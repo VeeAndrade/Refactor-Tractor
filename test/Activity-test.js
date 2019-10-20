2c704b44fc26fdb2e3355481c59817d7ac2659e1
sertmer <scottertmer@gmail.com>
2019-10-20 11:57:39 -0600

    Dry up ActivityRepo by changing two functions (getPositive/NegativeStepTrends) into one, with a boolean switch

--- test/Activity-test.js
+++ test/Activity-test.js
@@ -141,11 +141,9 @@ describe('ActivityRepository', () => {
       ]);
   });
 
-  it('should return the positive trend dates', () => {
-    expect(activityRepository.getPositiveStepTrends()).to.deep.equal(['2019/08/20', '2019/08/23'])
+  it('should return the positive or negative trend dates', () => {
+    expect(activityRepository.getStepTrends(true)).to.deep.equal(['2019/08/20', '2019/08/23']);
+    expect(activityRepository.getStepTrends(false)).to.deep.equal([])
   });
 
-  it('should return the negative trend dates', () => {
-    expect(activityRepository.getNegativeStepTrends()).to.deep.equal([]);
-  });
 });
