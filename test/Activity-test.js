5d087ea44c6061f9ece1d08060b7d3f2f8187bc1
sertmer <scottertmer@gmail.com>
2019-10-29 16:13:56 -0600

    Set up spy for return avg stairs climbed by date test

--- test/Activity-test.js
+++ test/Activity-test.js
@@ -110,6 +110,13 @@ describe('ActivityRepository', () => {
   });
 
   it('should return the average number of stairs climbed for all users on a given date', () => {
+    chai.spy.on(activityRepository, 'getAllUserInfoByDate', () => {
+      return [
+        {"userID": 13, "date": "2019/08/25", "numSteps": 9352, "minutesActive": 567, "flightsOfStairs": 143},
+        { "userID": 5, "date": "2019/08/25", "numSteps": 6425, "minutesActive": 67, "flightsOfStairs": 42},
+        { "userID": 6, "date": "2019/08/25", "numSteps": 842, "minutesActive": 43, "flightsOfStairs": 86 } 
+      ];
+    })
     expect(activityRepository.getAverages('2019/08/25', 'flightsOfStairs')).to.equal(90);
   });
 
